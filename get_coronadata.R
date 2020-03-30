4cd0d8736fa18622655dfb4ef8373faaf9338960
Brian Peña Calero <brianmsm@gmail.com>
2020-03-30 16:10:56 -0500

    Update get_coronadata.R
    Update and Fixs:
    - Fix Date
    - Update code
    - Add lubridate as dependency

--- get_coronadata.R
+++ get_coronadata.R
@@ -24,21 +24,40 @@ get_coronadata <- function(show.na = FALSE) {
     install.packages("xml2") 
   } 
   
+  if(!"lubridate" %in% rownames(installed.packages())) {
+    install.packages("lubridate") 
+  } 
+  
   `%>%` <- magrittr::`%>%`
   
+  # Extract data
   freq_coronavirus <- xml2::read_html("https://www.worldometers.info/coronavirus/") %>% 
     rvest::html_nodes("table") %>% 
     rvest::html_table() %>% 
-    magrittr::extract2(1)
-  
-  freq_coronavirus <- freq_coronavirus %>% 
-    dplyr::mutate(NewCases = stringr::str_remove(NewCases, "[+]")) %>% 
-    dplyr::mutate_at(dplyr::vars(TotalCases:TotalDeaths, 
-                   TotalRecovered:`Serious,Critical`),
-              ~ stringr::str_replace(., ",", "")) %>% 
-    dplyr::mutate_at(dplyr::vars(-`Country,Other`), as.numeric) %>% 
+    dplyr::bind_rows(.id = "Date_extract") %>% 
     tibble::as_tibble()
   
+  # Format date 
+  freq_coronavirus <- freq_coronavirus %>% 
+    dplyr::mutate(
+      Date_extract = dplyr::case_when(
+        Date_extract == 1 ~ lubridate::ymd(Sys.Date()),
+        TRUE ~ lubridate::ymd(Sys.Date()) -1
+      ),
+      `Reported1st case` = paste0("2020 ", `Reported1st case`, sep = ""),
+      `Reported1st case` = lubridate::ymd(`Reported1st case`)
+    ) 
+  
+  # Format numbers
+  freq_coronavirus <- freq_coronavirus %>% 
+    dplyr::mutate_at(dplyr::vars(NewCases, NewDeaths),
+                     ~ stringr::str_remove(., "[+]")) %>% 
+    dplyr::mutate_at(dplyr::vars(TotalCases:`Deaths/1M pop`),
+                     ~ stringr::str_replace(., ",", "")) %>% 
+    dplyr::mutate_at(dplyr::vars(-c(Date_extract, `Country,Other`,
+                                    `Reported1st case`)), as.numeric) 
+  
+  # Add NA or not
   if(show.na == FALSE) {
     freq_coronavirus <- freq_coronavirus %>% 
       dplyr::mutate_all(~ replace(., is.na(.), 0)) 
